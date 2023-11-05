849244616c0ae28fbf3b444742cc534ac1acc965
Fatima Adnan <143820850+FatimaAdnan@users.noreply.github.com>
2023-11-04 23:46:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var headline = document.querySelector('#tagline')
+headline.style.color = 'yellow';
+
+var items = docment.querySelector('.li:nthchild(odd)');
+for(var i=0; i<.length; i++){
+    even[i].style.backgroundColor = '#f4f4f4'
+}
+items[2].style.color ='red';
+var input = document.querySelector('input');
+input.value = ' Enter Your Name';
+var button = document.querySelector('input[type="submit"]');
+button.style.backgroundColor = 'red';
+button.style.color = 'yellow';
+button.value = 'SEND';
